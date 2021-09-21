30cf9e6fe6fc16651d1fcfd417380e35749fddf6
Erlan Abdraimov <era.ab.02@gmail.com>
2021-09-21 22:18:54 +0300

    end with HTML 'Base HTML'

--- movies/urls.py
+++ movies/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import MovieView
+from .views import MovieView, MovieDetail
 
 urlpatterns = [
     path('', MovieView.as_view(), name='home'),
+    path('detail/<slug:slug>', MovieDetail.as_view(), name='detail_movie')
 ]
--- movies/views.py
+++ movies/views.py
@@ -1,13 +1,25 @@
-from django.shortcuts import render
-from django.views.generic import View
+from django.shortcuts import render, get_object_or_404
+from django.views.generic import View, DetailView, ListView
 
 from movies.models import Movie
 
 
-class MovieView(View):
+class MovieView(ListView):
     """ Список фильмов """
+    queryset = Movie.objects.all()
+    model = Movie
+    template_name = 'movies/movies.html'
+    context_object_name = 'movies'
 
-    def get(self, request):
-        movies = Movie.objects.all()  # SELECT * FROM movies_movie
-        data = {'movies': movies}
-        return render(request, 'movies/movies.html', context=data)
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super(MovieView, self).get_context_data(**kwargs)
+        context['last_movies'] = Movie.objects.order_by('year')
+        return context
+
+
+class MovieDetail(DetailView):
+    """ Полное описание фильма """
+    model = Movie
+    slug_field = 'url'
+    context_object_name = 'movie'
+    template_name = 'movies/movie_detail.html'
